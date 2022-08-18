a9ce07c281a475a033d80f01845f890c2cec4a77
ZINTHUHTET-P000\ZinThuHtet <scm.zinthuhtet@gmail.com>
2022-08-18 15:02:59 +0630

    - template and component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DebugService } from './debug.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit { 
+  title = 'my-app'; 
+  constructor(private debugService : DebugService) {} ngOnInit() { 
+     this.debugService.info("Angular Application starts"); 
+  } 
 }
