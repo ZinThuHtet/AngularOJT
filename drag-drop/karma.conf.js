7a7ba0bf5fcbef27e920f58ec42b33bf20c1426c
ZINTHUHTET-P000\ZinThuHtet <scm.zinthuhtet@gmail.com>
2022-09-13 09:38:21 +0630

    - initial commit

--- drag-drop/karma.conf.js
+++ drag-drop/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/parent-child'),
+      dir: require('path').join(__dirname, './coverage/drag-drop'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- drag-drop/src/app/app.component.spec.ts
+++ drag-drop/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parent-child'`, () => {
+  it(`should have as title 'drag-drop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('parent-child');
+    expect(app.title).toEqual('drag-drop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parent-child app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('drag-drop app is running!');
   });
 });
